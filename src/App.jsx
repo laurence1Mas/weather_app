ab3e97e2a52e06bfc8d648f0e737f0bfed388029
laurence1Mas <119684303+laurence1Mas@users.noreply.github.com>
2026-02-02 14:43:13 +0100

    fix:update of the header part to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   return (
     <>
       <Header />
+      <br /><br />
       {/* here is the main content of the page */}
-      <section style={{ backgroundImage: "url('/bg.jpg')" }} className="main-content bg-cover bg-center min-h-screen flex flex-col items-center justify-center"
-      ><br />
+      <section style={{ backgroundImage: "url('/bg.jpg')" }} className="main-content bg-cover bg-center min-h-screen flex flex-col items-center justify-center pt-24"
+      >
         <div className="max-w-7xl w-full mx-auto p-8 md:p-12 rounded-2xl bg-black/50 backdrop-blur-md shadow-[0_0_40px_rgba(156,163,175,0.35),0_25px_50px_rgba(0,0,0,0.45)] border">
           <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
 
@@ -36,8 +37,8 @@ function App() {
 
         <div className="max-w-7xl w-full mx-auto p-8 md:p-12 ">
           <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4">
-            <div className="text-center md:text-left space-y-4 rounded-2xl bg-black/50 backdrop-blur-md shadow-[0_0_40px_rgba(156,163,175,0.35),0_25px_50px_rgba(0,0,0,0.45)]">
-              <h3 className="text-white">Weather details</h3>
+            <div className="text-center md:text-left space-y-4 rounded-2xl bg-black/50 backdrop-blur-md shadow-[0_0_40px_rgba(156,163,175,0.35),0_25px_50px_rgba(0,0,0,0.45)] p-6">
+              <h3 className="text-white text-xl font-bold mb-6">Weather details</h3>
               <div className="mt-8 rounded-2xl bg-black/50 backdrop-blur-md shadow-[0_0_40px_rgba(156,163,175,0.35),0_25px_50px_rgba(0,0,0,0.45)] border">
                 <div className="grid grid-cols-2 md:grid-cols-3 gap-3 p-4 text-white">
                   <div className="text-center bg-gray-800 p-4 rounded">
@@ -99,11 +100,6 @@ function App() {
                       <span className="text-xl font-bold">26°</span>
                       <span className="text-gray-300 ml-2">20°</span>
                     </div>
-                  </div>
-
-                  {/* Jour 3 à 5 - répéter la même structure */}
-                  <div className="flex items-center justify-between text-white p-4 rounded-xl bg-gray-800/50">
-                    {/* ... */}
                   </div>
                 </div>
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
 
 import React from "react";
 
-function Header({city,setCity, onSearch, onLocate}) {
+function Header({ city, setCity, onSearch, onLocate }) {
 
     return (
-        <header className="bg-blue-600 text-white p-4 shadow-md ">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-blue-600 text-white p-4 shadow-md ">
             <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-4">
                 <h1 className="text-2xl font-bold flex items-center gap-2">
                     Weather App
